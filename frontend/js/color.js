d8eb6a3c102ca9beb110c43208dd2ce103d564ff
Gouthamreddy2000 <mandlagouthamreddy@gmail.com>
2021-05-06 14:27:05 +0530

    color picker

--- frontend/js/color.js
+++ frontend/js/color.js
@@ -1,16 +1,14 @@
-
 function func1(val){
-document.getElementById("two").innerHTML=val;
-document.getElementById("color").style.backgroundColor="rgba(val,150,300,0.5)";
-document.getElementById("a").innerHTML=val;
-}
-function func2(val){
-document.getElementById("four").innerHTML=val;
-document.getElementById("color").style.backgroundColor="rgba(100,val,300,0.5)";
-document.getElementById("b").innerHTML=val;
-}
-function func3(val){
-document.getElementById("six").innerHTML=val;
-document.getElementById("color").style.backgroundColor="rgba(300,250,val,0.5)";
-document.getElementById("c").innerHTML=val;
+z=document.getElementById("color")
+a=document.getElementById("one");
+b=document.getElementById("three");
+c=document.getElementById("five");
+let rgb="rgb("+a.value+","+b.value+","+c.value+")";
+z.style.backgroundColor=rgb;
+document.getElementById("two").innerHTML=a.value;
+document.getElementById("four").innerHTML=b.value;
+document.getElementById("six").innerHTML=c.value;
+document.getElementById("a").innerHTML=a.value;
+document.getElementById("b").innerHTML=b.value;
+document.getElementById("c").innerHTML=c.value;
 }
