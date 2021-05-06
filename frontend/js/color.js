cb8c553e7b42e420d26d703bf3d043f4fc343f58
Gouthamreddy2000 <mandlagouthamreddy@gmail.com>
2021-05-06 08:17:03 +0530

    updated color

--- frontend/js/color.js
+++ frontend/js/color.js
@@ -1,12 +1,15 @@
 function func1(val){
 document.getElementById("two").innerHTML=val;
 document.getElementById("color").style.backgroundColor="rgba(val,150,300,0.5)";
+document.getElementById("a").innerHTML=val;
 }
 function func2(val){
 document.getElementById("four").innerHTML=val;
 document.getElementById("color").style.backgroundColor="rgba(100,val,300,0.5)";
+document.getElementById("b").innerHTML=val;
 }
 function func3(val){
 document.getElementById("six").innerHTML=val;
 document.getElementById("color").style.backgroundColor="rgba(300,250,val,0.5)";
+document.getElementById("c").innerHTML=val;
 }
