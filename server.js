8d7d9c34ee69c82ef43a0c61cfacd8b7b589b809
Gouthamreddy2000 <mandlagouthamreddy@gmail.com>
2021-05-19 08:55:50 +0530

    added mongoose

--- server.js
+++ server.js
@@ -1,76 +1,123 @@
 const express = require('express');
- const bodyParser= require('body-parser');
+//const courseLib =require('./backend/lib/courselib.js');
+const table = require('./table')
+const bodyParser = require('body-parser');
 const app = express();
+var mongoose = require('mongoose');
+//var password=process.env.Mongo_atlas_password;
+var connectionString = "mongodb+srv://Goutham:Jyothi@123@cluster0.zv3at.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('connected', function () {
+    console.log("database connected");
+});
 const crypto = require("crypto");
-let request=0;
-const todo=[
+let request = 0;
+const todo = [
 
 ];
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(function(req,res,next){
-console.log("req came");
-request++;
-next();
+app.use(function (req, res, next) {
+    console.log("req came");
+    request++;
+    next();
 });
-var arr=[{"name":"goutham","email":"mandlagouthamreddy@gmail.com","userid":1}]
-app.use(express.static(__dirname+"/frontend"));
-app.get("/resume",function(req,res){
-let indexs=__dirname+"/frontend/html/resume.html";
-res.sendFile(indexs);
+app.use(express.static(__dirname + "/frontend"));
+app.get("/resume", function (req, res) {
+    let indexs = __dirname + "/frontend/html/resume.html";
+    res.sendFile(indexs);
 });
-app.get("/search",function(req,res){
-let indexs=__dirname+"/frontend/html/search.html";
-res.sendFile(indexs);
+app.get("/search", function (req, res) {
+    let indexs = __dirname + "/frontend/html/search.html";
+    res.sendFile(indexs);
 });
-app.get("/",function(req,res){
-let indexs=__dirname+"/frontend/html/basic.html";
-res.sendFile(indexs);
+app.get("/", function (req, res) {
+    let indexs = __dirname + "/frontend/html/basic.html";
+    res.sendFile(indexs);
 });
-app.get("/apple",function(req,res){
-let indexs=__dirname+"/frontend/html/apple.html";
-res.sendFile(indexs);
+app.get("/apple", function (req, res) {
+    let indexs = __dirname + "/frontend/html/apple.html";
+    res.sendFile(indexs);
 });
-app.get("/todo",function(req,res){
-let indexs=__dirname+"/frontend/html/todo.html";
-res.sendFile(indexs);
+app.get("/todo", function (req, res) {
+    let indexs = __dirname + "/frontend/html/todo.html";
+    res.sendFile(indexs);
 });
-app.post("/api/users",function(req,res){
-const course={
-id:todo.length+1,
-name:req.body
-};
-todo.push(course);
-res.json(todo);
+app.get("/crud", function (req, res) {
+    let indexs = __dirname + "/frontend/html/crud.html";
+    res.sendFile(indexs);
 });
-app.delete("/api/users/:id",function(req,res){
-const to=todo.find(c=>c.id===parseInt(req.params.id));
-var user=req.params.userid;
-const index=todo.indexOf(to);
-todo.splice(index,1);
-res.send(todo);
+//app.get('/api/courses', courseLib.getallcourses);
+app.get("/color", function (req, res) {
+    let indexs = __dirname + "/frontend/html/color.html";
+    res.sendFile(indexs);
 });
-app.get("/color",function(req,res){
-let indexs=__dirname+"/frontend/html/color.html";
-res.sendFile(indexs);
+app.get("/login", function (req, res) {
+    let indexs = __dirname + "/frontend/html/login.html";
+    res.sendFile(indexs);
 });
- app.get("/login",function(req,res){
-let indexs=__dirname+"/frontend/html/login.html";
-res.sendFile(indexs);
+app.get("/register", function (req, res) {
+    let indexs = __dirname + "/frontend/html/registration.html";
+    res.sendFile(indexs);
 });
-app.get("/register",function(req,res){
-let indexs=__dirname+"/frontend/html/registration.html";
-res.sendFile(indexs);
+app.get("/piechart", function (req, res) {
+    let indexs = __dirname + "/frontend/html/piechart.html";
+    res.sendFile(indexs);
 });
-app.get("/piechart",function(req,res){
-let indexs=__dirname+"/frontend/html/piechart.html";
-res.sendFile(indexs);
-});
- 
+
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
- 
+
 // Start the server
-app.listen(PORT, function(){
-    console.log("Server Starting running on http://localhost:"+PORT);
+app.listen(PORT, function () {
+    console.log("Server Starting running on http://localhost:" + PORT);
+})
+ a={
+    "task":[]
+   };
+app.get('/crud/get', function(req, res){
+    table.find()
+    .then((result) =>{
+        res.send(result);
+        console.log(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
+app.post('/crud/post', function(req, res){
+    var newUser= req.body;
+    const newTable = new table({
+        name : newUser.name,
+        Articels : newUser.Articels
+    })
+    console.log(newTable);
+    newTable.save();
+ })
+
+ app.delete('/crud/del/:id', function(req, res){
+    var i=req.params.id
+    table.findByIdAndDelete(i, (err)=>{
+        if(err){
+            console.log('Error:'+err);
+        }
+        else{
+            console.log('Success');
+        }
+    })
+})
+
+app.put('/crud/put/:id', function(req, res){
+    var i=req.params.id
+    table.findById(i, function (err,Obj) {
+        if(err){
+            console.log('Error:' + err);
+        }
+        else{
+            table.findByIdAndUpdate(i, {Articels: value}, function(){})
+        };
+    });
+
+    
 })
---
+++ testdb.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var connectionString = "mongodb+srv://Goutham:Jyothi@123@cluster0.zv3at.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('connected', function() {
+    console.log("database connected");
+})
+
+const courselib = require('./backend/lib/courselib');
+courselib.createcourse({ coursename: 'meanstack course' }, function(err, course) {
+    console.log(course);
+})
